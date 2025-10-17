eb4370628abb0600ac2c13fa36f595b0c3383334
akley <akley-MK4@outlook.com>
2025-10-17 16:29:06 +0800

    Support webhook (#4)
    * Optimized node checking for replica set
    
    * Added the CRD to control a mongodb shard
    
    * Updated for sample config
    
    * Updated the use of mongodb client
    
    * Added the new CRD MgoDataReplicaSet to control the mongo replica set
    
    * Added conversion to replica set
    
    * Optimized the oeration of reconcile
    
    * Updated data rs status
    
    * Adjusted the registration of DataRS
    
    * upd
    
    * upd
    
    * upd
    
    * upd
    
    * upd
    
    * upd
    
    * upd
    
    * upd

--- internal/controller/mgodatareplicaset_controller.go
+++ internal/controller/mgodatareplicaset_controller.go
@@ -58,6 +58,8 @@ type MgoDataReplicaSetReconciler struct {
 // +kubebuilder:rbac:groups=mongodb.akleymk4.com,resources=mgodatareplicasets/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=mongodb.akleymk4.com,resources=mgodatareplicasets/finalizers,verbs=update
 // +kubebuilder:rbac:groups=mongodb.akleymk4.com,resources=mongodbclusters/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=apps,resources=statefulsets;deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
--- internal/controller/mongodbcluster_controller.go
+++ internal/controller/mongodbcluster_controller.go
@@ -41,6 +41,8 @@ type MongoDBClusterReconciler struct {
 // +kubebuilder:rbac:groups=mongodb.akleymk4.com,resources=mongodbclusters,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=mongodb.akleymk4.com,resources=mongodbclusters/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=mongodb.akleymk4.com,resources=mongodbclusters/finalizers,verbs=update
+// +kubebuilder:rbac:groups=apps,resources=statefulsets;deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
